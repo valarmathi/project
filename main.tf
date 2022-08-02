2a0f64af8c38f1374219b26693b8a13528226e47
valarmathi <valarkannan@gmail.com>
2022-08-02 13:36:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,76 @@
-resource "random_id" "server" {
-  byte_length = 8
+resource "azurerm_resource_group" "main" {
+  name     = "${var.resourceGroup}"
+  location = "${var.location}"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "tfeautorgdnd-${random_id.server.hex}"
-  location = "${var.region}"
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["${var.address_space}"]
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
 }
 
-resource "azurerm_storage_account" "stor" {
-  name                     = "stor${random_id.server.hex}"
-  location                 = "${var.region}"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
-  account_tier             = "${var.storage_account_tier}"
-  account_replication_type = "${var.storage_replication_type}"
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "${var.subnet_prefix}"
 }
 
-resource "azurerm_storage_account1" "stor" {
-  name                     = "stor${random_id.server.hex}"
-  location                 = "${var.region}"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
-  account_tier             = "${var.storage_account_tier}"
-  account_replication_type = "${var.storage_replication_type}"
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  ip_configuration {
+    name                          = "${var.prefix}-ipconfiguration"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_virtual_machine" "main" {
+  name			= "Check1"
+  location              = "${azurerm_resource_group.main.location}"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "${var.vmSize}"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "${var.image_publisher}"
+    offer     = "${var.image_offer}"
+    sku       = "${var.image_sku}"
+    version   = "${var.image_version}"
+  }
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.hostname}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
 
-resource "azurerm_storage_account2" "stor" {
-  name                     = "stor${random_id.server.hex}"
-  location                 = "${var.region}"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
-  account_tier             = "${var.storage_account_tier}"
-  account_replication_type = "${var.storage_replication_type}"
+resource "azurerm_public_ip" "test" {
+  name                = "${var.prefix}-PublicIp"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "staging"
+  }
 }
