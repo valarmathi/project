45a24086634c6079cb989e39f4a4939846e723ef
valarmathi <valarkannan@gmail.com>
2022-08-02 13:35:50 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,87 +1,27 @@
-variable "resource_group" {
-  description = "The name of the resource group in which to create the virtual network."
-  default="tfeautorgdnd"
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "defaultvalue"
 }
 
-variable "rg_prefix" {
-  description = "The shortened abbreviation to represent your resource group that will go on the front of some resources."
-  default     = "rg"
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created"
 }
 
-variable "hostname" {
-  description = "VM name referenced also in storage-related names."
-  default="tf"
+variable "test2" {
+  description = "administrator user name"
+  default     = "testvar"
 }
 
-variable "dns_name" {
-  description = " Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
-  default="vm"
-}
 
-variable "lb_ip_dns_name" {
-  description = "DNS for Load Balancer IP"
-  default="lb1"
-}
-
-variable "vm_count_per_subnet" {
-	description = "count per subnet"
-	default=2
-}
-
-variable "region" {
-  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
-}
-
-variable "virtual_network_name" {
-  description = "The name for the virtual network."
-  default     = "vnet"
-}
-
-variable "address_space" {
-  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_prefix" {
-  description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
-}
-
-variable "storage_account_tier" {
-  description = "Defines the Tier of storage account to be created. Valid options are Standard and Premium."
-  default     = "Standard"
-}
-
-variable "storage_replication_type" {
-  description = "Defines the Replication Type to use for this storage account. Valid options include LRS, GRS etc."
-  default     = "LRS"
-}
-
-variable "vm_size" {
+variable "subscriptionId" {}
+variable "clientId" {}
+variable "clientSecret" {}
+variable "resourceGroup" {}
+variable "tenantId" {}
+variable "vmSize" {
   description = "Specifies the size of the virtual machine."
   default     = "Standard_D1_v2"
 }
-
-variable "image_publisher" {
-  description = "name of the publisher of the image (az vm image list)"
-  default     = "MicrosoftWindowsServer"
-}
-
-variable "image_offer" {
-  description = "the name of the offer (az vm image list)"
-  default     = "WindowsServer"
-}
-
-variable "image_sku" {
-  description = "image sku to apply (az vm image list)"
-  default     = "2012-R2-Datacenter"
-}
-
-variable "image_version" {
-  description = "version of the image to apply (az vm image list)"
-  default     = "latest"
-}
-
 variable "admin_username" {
   description = "administrator user name"
   default     = "vmadmin"
@@ -91,11 +31,34 @@ variable "admin_password" {
   description = "administrator password (recommended to disable password auth)"
   default = "admin01!"
 }
-
-variable "environment" {
-  description = "tag name for vm"
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
+}
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
+}
+variable "image_publisher" {
+  description = "name of the publisher of the image (az vm image list)"
+  default     = "Canonical"
 }
 
-variable "environment12" {
-  description = "tag name for vm"
+variable "image_offer" {
+  description = "the name of the offer (az vm image list)"
+  default     = "UbuntuServer"
+}
+
+variable "image_sku" {
+  description = "image sku to apply (az vm image list)"
+  default     = "16.04-LTS"
+}
+
+variable "image_version" {
+  description = "version of the image to apply (az vm image list)"
+  default     = "latest"
+}
+variable "hostname" {
+  description = "VM name referenced also in storage-related names."
+  default="tf"
 }
