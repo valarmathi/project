506ec70e5669629766d721e3b7bfbc68a96cf85d
valarmathi <valarkannan@gmail.com>
2022-08-02 16:12:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_network_interface" "main" {
   }
 }
 resource "azurerm_virtual_machine" "main" {
-  name			= "Check1"
+  name			= "AzureVMFromTFE"
   location              = "${azurerm_resource_group.main.location}"
   resource_group_name   = "${azurerm_resource_group.main.name}"
   network_interface_ids = ["${azurerm_network_interface.main.id}"]
